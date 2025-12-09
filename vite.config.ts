505b14da8e704fb23d09201fb09a650cb9296b47
isaelsolis <isolis@lafril.com.ec>
2025-12-09 16:50:51 -0500

    habilitar host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['lunaserena2.onrender.com'] // agregado para permitir ese host
       },
       plugins: [react()],
       define: {
